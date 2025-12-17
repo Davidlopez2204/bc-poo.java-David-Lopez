29baa80146b0cbd960232ddb49b4780c035abe0d
Davidlopez2204 <Davidlopez22042007@gmail.com>
2025-12-17 00:53:51 -0500

    feat(semana-04): Implementar herencias en Compañia de Seguros

--- semana4/src/co/edu/sena/Semana4/Main.java
+++ semana4/src/co/edu/sena/Semana4/Main.java
@@ -2,10 +2,10 @@ package co.edu.sena.Semana4;
 
 public class Main {
     public static void main(String[] args) {
-        PolizadeSeguro[] polizas = new PolizadeSeguro[]{
-                new SegurodeVida(6, "seguro de vida", "Ricardo Montañez", true, 1000000.0, 50, "Masculino"),
-                new SegurodeCoche(7, "seguro de coche", "Naibeth Morales", false, 800000.5, "camaro ss", 2025)
-        };
+        PolizadeSeguro[] polizas = new PolizadeSeguro[2];
+             polizas[0] =  new SegurodeVida(6, "seguro de vida", "Ricardo Montañez", true, 1000000.0, 50, "Masculino");
+             polizas[1] =  new SegurodeCoche(7, "seguro de coche", "Naibeth Morales", false, 800000.5, "camaro ss", 2025);
+
 
         for (PolizadeSeguro pol : polizas) {
             System.out.println("----------------");
